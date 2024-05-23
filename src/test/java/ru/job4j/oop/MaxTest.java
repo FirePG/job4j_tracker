6546124e51906cc44615c8c3462a20a27c66be16
FirePG <paul1992.92@inbox.ru>
2024-05-23 21:49:32 +0700

    3. Перегрузить метод max для трех чисел [#122644]

--- src/test/java/ru/job4j/oop/MaxTest.java
+++ src/test/java/ru/job4j/oop/MaxTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.oop;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MaxTest {
@@ -18,7 +19,7 @@ class MaxTest {
         int first = 3;
         int second = 2;
         int third = 5;
-        int result = new Max().max(first, second,third);
+        int result = new Max().max(first, second, third);
         int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
@@ -29,7 +30,7 @@ class MaxTest {
         int second = 3;
         int third = 2;
         int four = 1;
-        int result = new Max().max(first, second,third,four);
+        int result = new Max().max(first, second, third, four);
         int expected = 3;
         assertThat(result).isEqualTo(expected);
     }
