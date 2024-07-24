5f5bd3b24313be7d333692ec554bf0a9428f50be
FirePG <paul1992.92@inbox.ru>
2024-07-24 20:28:07 +0700

    8. Что такое валидация? [#246864 #506403]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,7 +60,6 @@ public class Tracker {
         if (result) {
             item.setId(id);
             items[index] = item;
-            result = true;
         }
         return result;
     }
@@ -72,7 +71,6 @@ public class Tracker {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
-            result = true;
         }
         return result;
     }
